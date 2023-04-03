c58587ad1c9521ff7260e013127436c0124b9167
stanpai78 <stanpai78@gmail.com>
2023-04-03 18:54:34 +0800

    Added user model and controller

---
+++ src/controllers/usersController.js
@@ -0,0 +1,34 @@
+import User from "../models/user.js";
+
+const usersController = {
+    /**
+     * Expects email and password
+     * Usage:
+     * fetch(`/api/auth?email=${email_var}&password=${password_var}`) // async use .then()
+     */
+    authenticateUser: function(req, res) {
+        const email = req.query.email;
+        const pass = req.query.password;
+        User.findOne({email:email, password:pass}).lean().exec().then(result => {
+            if (result == null) {
+                res.json('');
+            } else {
+                res.json(result);
+            }
+        });
+    },
+
+    /**
+     * Creates new user in database
+     * TODO: validation
+     */
+    postUser: function(req, res) {
+        User.create(req.body);
+    }
+
+    /**
+     * TODO: Past Registrations
+     */
+}
+
+export default usersController;
