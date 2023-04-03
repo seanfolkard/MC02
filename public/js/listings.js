95aa015dc4fdf92eec16f2e3277fce6fde331265
stanpai78 <stanpai78@gmail.com>
2023-04-04 01:05:32 +0800

    Fixed venue modals

--- public/js/listings.js
+++ public/js/listings.js
@@ -2,41 +2,43 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     fetch('/api/venues').then(response => response.text()).then(data => {
         document.querySelector("#here").innerHTML = data;
-    })
+        // console.log(data);
+        var btn = document.querySelectorAll("button.modal-button");
 
-    var btn = document.querySelectorAll("button.modal-button");
+        // All page modals
+        var modals = document.querySelectorAll('.modal');
 
-    // All page modals
-    var modals = document.querySelectorAll('.modal');
+        // Get the <span> element that closes the modal
+        var spans = document.getElementsByClassName("close");
 
-    // Get the <span> element that closes the modal
-    var spans = document.getElementsByClassName("close");
-
-    // When the user clicks the button, open the modal
-    for (var i = 0; i < btn.length; i++) {
-     btn[i].onclick = function(e) {
-        e.preventDefault();
-        const modal = document.querySelector(e.target.getAttribute("href"));
-        modal.style.display = "block";
-        console.log('test');
-     }
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    for (var i = 0; i < spans.length; i++) {
-     spans[i].onclick = function() {
-        for (var index in modals) {
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-     }
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target.classList.contains('modal')) {
-         for (var index in modals) {
-          if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        // When the user clicks the button, open the modal
+        console.log(btn.length);
+        for (var i = 0; i < btn.length; i++) {
+         btn[i].onclick = function(e) {
+            e.preventDefault();
+            const modal = document.querySelector(e.target.getAttribute("href"));
+            modal.style.display = "block";
+            console.log('test');
          }
         }
-    }
+
+        // When the user clicks on <span> (x), close the modal
+        for (var i = 0; i < spans.length; i++) {
+         spans[i].onclick = function() {
+            for (var index in modals) {
+                if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+            }
+         }
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target.classList.contains('modal')) {
+             for (var index in modals) {
+              if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+             }
+            }
+        }
+    })
+
 });
--- src/controllers/venuesController.js
+++ src/controllers/venuesController.js
@@ -16,13 +16,31 @@ const venuesController = {
                         element['noStar'].push({});
                     }
                 }
+                const reviews = element['reviews'];
+                const currYear = new Date().getFullYear();
+                for (let index = 0; index < reviews.length; index++) {
+                    const review = reviews[index];
+                    const revYear = new Date(review['date']).getFullYear();
+                    const newYear = currYear - revYear;
+                    if (newYear == 0) {
+                        review['date'] = 'this year'
+                    } else if (newYear == 1) {
+                        review['date'] = 'a year ago'
+                    } else {
+                        review['date'] = newYear + ' years ago'
+                    }
+                }
             }
-            venues['layout'] = false;
-            console.log(venues);
             res.render('partials/venue', {
-                venues: venues
+                venues: venues,
+                layout: false
             });
         });
+    },
+
+    fillUp: function(req, res) {
+        Venue.findOne({_id: req.query._id}).lean().exec()
+            // .then(result => )
     }
 };
 
